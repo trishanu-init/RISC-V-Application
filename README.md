7401c0d192f4e2ff46afb41762075949907d3d55
trishanu-init <trishanu.nayak@outlook.com>
2024-01-30 22:44:23 +0530

    added initial files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # RISC-V-Application
 Coding Challenge Submission for LFX Mentorship 2024 under RISC-V Sailing Downstream
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/trishanu-init/RISC-V-Application.git
+```
+
+Go to the project directory
+
+```bash
+  cd RISC-V-Application
+```
+Install pytest
+
+```cmd
+  pip install pytest
+```
+Run included tests in test_list.py
+```cmd
+  pytest -s --verbose
+```
---
+++ list_operation.py
@@ -0,0 +1,24 @@
+class InvalidListSize(Exception):
+    "Raised when the input string length is not a multiple of 10"
+    pass
+def process_user_list(input):
+    try:
+        user_list = input.split()
+        size=len(user_list)
+        if (size%10!=0 or size==0) :
+            raise InvalidListSize
+        else:
+            for i in range(size):
+            # convert each item to int type
+                user_list[i] = int(user_list[i])
+            #remove values from indices which are multiple of 2 or 3
+            new_list = [value for index, value in enumerate(user_list) if (index + 1) % 2 != 0 and (index + 1) % 3 != 0]
+            print("New  list :",new_list)
+
+    except InvalidListSize:
+     print("Error: List size is not a multiple of 10")
+    except ValueError:
+        print("The input was not a valid integer.")
+     
+input_string = input('Enter elements of a list separated by space :')
+process_user_list(input_string)
